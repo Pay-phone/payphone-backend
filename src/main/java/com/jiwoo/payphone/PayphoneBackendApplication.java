182324e47a2a2b407e7f60359074e05fe3cd05b2
Jiwoo <emily1290@naver.com>
2024-12-17 11:44:56 +0900

    feat: Payphone Backend 프로젝트 초기 설정

---
+++ src/main/java/com/jiwoo/payphone/PayphoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jiwoo.payphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayphoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayphoneBackendApplication.class, args);
+	}
+
+}
